650d80d3f4948c9cfe5062e1bc1fc202a9be7edf
jong-ah <jonga6431@gmail.com>
2021-06-19 10:18:44 +0900

    remote test

--- 박종아/210621_BFS_DFS_공통.js
+++ 박종아/210621_BFS_DFS_공통.js
@@ -1,1 +1,19 @@
 console.log('remote test');
+
+/*
+ Github workflow
+
+ <시작>
+ 01. Fork <최신 업데이트한 repository>
+ 02. 개인 폴더 안에 js파일 만들어 코드 적고, 커밋(기록) 하기
+ -> 파일명 '스터디날짜_주제_공통(or 개인).js' ex) 210621_BFS_DFS_공통.js, 210621_BFS_DFS_개인.js
+ -> 커밋 '문제명' ex) 프로그래머스 레벨3 네트워크, 백준 1234 바나나
+ 03. git push origin main // 푸쉬하고 업데이트되었으면 디스크드로 알려주기
+
+ <remote 설정>
+ 04. git remote -v // 내 주소는 있음 확인
+ 05. git remote add PJA <PJA remote repository> // 스터디원 remote 추가
+ 06. git pull PJA main // 최신 업데이트된 스터디원의 작업을 가져옴
+ 07. 02~03 반복 
+
+ */
