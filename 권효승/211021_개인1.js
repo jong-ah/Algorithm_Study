65880e3deeda124653be9ae98aa647bf04ff8d74
Hyoseung Kwon <idhyo0o@gmail.com>
2021-10-21 10:37:31 +0900

    프로그래머스 레벨3 최고의 집합

--- 권효승/211021_개인1.js
+++ 권효승/211021_개인1.js
@@ -3,18 +3,29 @@
 // 시간 복잡도
 /**********************************************************************
 n + s % n + n log n => O(n log n)??
+->
+개선 후 : O(s % n)
 ***********************************************************************/
 
 // 개선점
 /**********************************************************************
-
+굳이 sort 할 필요 없이 뒤에서 부터 숫자를 더해주면 훨씬 빠르다
 ***********************************************************************/
 
+// before
 function solution(n, s) {
   if (n > s) return [-1];
   const answer = new Array(n).fill(parseInt(s / n));
   for (let i = 0; i < s % n; i++) answer[i]++;
   return answer.sort((a, b) => a - b);
+}
+
+// after
+function solution(n, s) {
+  if (n > s) return [-1];
+  const answer = new Array(n).fill(parseInt(s / n));
+  for (let i = n - 1; i > n - 1 - (s % n); i--) answer[i]++;
+  return answer;
 }
 
 var n = 2;
